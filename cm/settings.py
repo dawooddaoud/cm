6c10e5c4f16236e9d55813c1153db98cedcf6d24
dawooddaoud <78720960+dawooddaoud@users.noreply.github.com>
2021-10-24 19:09:50 +0530

    Update settings.py

--- cm/settings.py
+++ cm/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dawood.daoud22@gmail.com'
-EMAIL_HOST_PASSWORD = 'da3or@da3or'
+EMAIL_HOST_USER = '***********************'
+EMAIL_HOST_PASSWORD = '*************************'
